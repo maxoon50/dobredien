5fa10374dfa5f1b99f3f32cd8d374254b5cea792
maxoon <desfouxmaxime@gmail.com>
2018-05-05 20:32:09 +0200

    add : condition online / offline

--- src/bo/User.ts
+++ src/bo/User.ts
@@ -1,10 +1,11 @@
 export class User {
-  
+
   private _pseudo: string;
   private _online: boolean;
 
-  constructor (pPseudo: string) {
+  constructor (pPseudo: string, pOnline: boolean) {
     this._pseudo = pPseudo;
+    this._online = pOnline;
   }
 
 
